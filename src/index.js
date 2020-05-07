c46d5c8db23cb2faa7ccf6f1f146fba5ab1e3926
Nokrei <dive877@gmail.com>
2020-05-07 19:45:52 +0400

    Add dotenv and data container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
+import "dotenv/config.js"
+import Axios from "axios";
+
+
+Axios.get("http://api.openweathermap.org/data/2.5/forecast?q=dubai&units=metric&APPID=" + process.env.API_KEY)
+.then((response => {
+  const weatherData = {
+   temp: response.data.list[0].main.temp,
+   hum: response.data.list[0].main.humidity,
+   lowTemp: response.data.list[0].main.temp_min,
+   highTemp: response.data.list[0].main.temp_max,
+   sunrise: new Date(response.data.city.sunrise* 1000).getHours() + ":" + new Date(response.data.city.sunrise* 1000).getMinutes(),
+   sunset: new Date(response.data.city.sunset* 1000).getHours() + ":" + new Date(response.data.city.sunset* 1000).getMinutes()
+  }
+  console.log("The current temperature is: " + weatherData.temp);
+}));
+
+let pos;
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+};
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  pos = [lat, lon];
+  console.log(pos);
+}
+
+getLocation()
+
 
 
 
@@ -8,3 +40,4 @@ import App from "./App"
    <App />,
     document.getElementById("root")
   );
+
